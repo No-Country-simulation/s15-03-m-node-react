649f7416d8f19226cd4615afde39fd7429762e37
Edivaldo Gomes <edslavianov@gmail.com>
2024-05-26 02:06:58 +0200

    Complete list to admin, pending, Older people, rejected people and actual residente people

--- client/src/components/AcceptPersonal/Pending.jsx
+++ client/src/components/AcceptPersonal/Pending.jsx
@@ -1,5 +1,8 @@
 import { FaCheck } from "react-icons/fa";
 import { IoCloseOutline } from "react-icons/io5";
+import OlderPersonal from "./OlderPersonal";
+import Rejected from "./Rejected";
+import Acepted from "./Acepted";
 
 export const data = [
   {
@@ -49,73 +52,119 @@ export const data = [
 function Pending() {
   return (
     <>
-      <div className="mx-auto px-4 sm:px-6 lg:px-8 mt-10">
-        <div className="overflow-x-auto">
-          <div className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
-            <table className="min-w-full divide-y divide-gray-200">
-              <thead className="bg-gray-50">
-                <tr>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Nombre
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Email
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Edificio
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Apartamento
-                  </th>
-                  <th
-                    scope="col"
-                    className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                  >
-                    Acciones
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="bg-white divide-y divide-gray-200">
-                {data &&
-                  data.map((item) => (
-                    <tr key={item.id} className="hover">
-                      <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 text-center">
-                        {item.name}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
-                        {item.email}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
-                        {item.building}
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
-                        {item.apartment}
-                      </td>
-                      <div className="flex items-center">
-                        <span className="mr-2 btn btn-outline btn-success btn-sm mt-2">
-                          <FaCheck size={20} />
-                        </span>
-                        <span className="btn btn-outline btn-error btn-sm mt-2">
-                          <IoCloseOutline size={20} />
-                        </span>
-                      </div>
+      <div role="tablist" className="tabs tabs-bordered">
+        <input
+          type="radio"
+          name="my_tabs_1"
+          role="tab"
+          className="tab"
+          aria-label="Pendentes"
+        />
+        <div role="tabpanel" className="tab-content p-10">
+          <div className="mx-auto px-4 sm:px-6 lg:px-8 mt-10">
+            <div className="overflow-x-auto">
+              <div className="min-w-full bg-white shadow-md rounded-lg overflow-hidden">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Nombre
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Edificio
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-center text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Apartamento
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Acciones
+                      </th>
                     </tr>
-                  ))}
-              </tbody>
-            </table>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {data &&
+                      data.map((item) => (
+                        <tr key={item.id} className="hover">
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 text-center">
+                            {item.name}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                            {item.email}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                            {item.building}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 text-center">
+                            {item.apartment}
+                          </td>
+                          <td>
+                            <span className="flex items-center">
+                              <span className="mr-2 btn btn-outline btn-success btn-sm mt-2">
+                                <FaCheck size={20} />
+                              </span>
+                              <span className="btn btn-outline btn-error btn-sm mt-2">
+                                <IoCloseOutline size={20} />
+                              </span>
+                            </span>
+                          </td>
+                        </tr>
+                      ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
           </div>
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_1"
+          role="tab"
+          className="tab"
+          aria-label="Aceptados"
+          checked
+        />
+        <div role="tabpanel" className="tab-content p-10">
+          <Acepted data={data} />
+        </div>
+
+        <input
+          type="radio"
+          name="my_tabs_1"
+          role="tab"
+          className="tab"
+          aria-label="Denegados"
+        />
+        <div role="tabpanel" className="tab-content p-10">
+          <Rejected data={data} />
+        </div>
+        <input
+          type="radio"
+          name="my_tabs_1"
+          role="tab"
+          className="tab"
+          aria-label="Antiguos"
+        />
+        <div role="tabpanel" className="tab-content p-10">
+          <OlderPersonal data={data} />
         </div>
       </div>
     </>
