3b3512fb5086b214747dca82b1be27f71770cbce
Lina Maria Cardona <linamaria126@gmail.com>
2024-05-26 21:04:48 -0500

    add Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Error404 from "./components/Error404";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -9,8 +10,13 @@ function App() {
         {/* Ruta principal de la app, no hace falta aclararla en el BrowserRouter */}
         <Route path="/" element={<Home />} />
 
+        {/* Ruta al Dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+
         {/* Cualquier otra ruta que nosotros no le demos al router de react va directamente a error y desde aca lo podemos mandar al home o algun otro lado */}
         <Route path="*" element={<Error404 />} />
+       
       </Routes>
     </>
   );
