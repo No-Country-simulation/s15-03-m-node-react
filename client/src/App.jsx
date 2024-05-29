eb5cdad36121da1393e5d6d94fbd928c1aad73a9
Facundo-Oliva <facundooliva500@hotmail.com>
2024-05-28 21:17:45 -0300

    add error 404

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Error404 from "./components/Error404";
+import Error404 from "./pages/Error404";
 import Board from "./pages/BusinessBoard";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
