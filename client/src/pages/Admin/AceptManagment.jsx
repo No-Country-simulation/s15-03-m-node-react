64fb1a67daf1d91264323cc155032f2b192b1acc
Edivaldo Gomes <edslavianov@gmail.com>
2024-06-03 20:24:39 +0200

    Refactor List admin

--- client/src/pages/Admin/AceptManagment.jsx
+++ client/src/pages/Admin/AceptManagment.jsx
@@ -1,7 +1,5 @@
-import Pending from "../../components/AcceptPersonal/Pending";
-import Acepted from "../../components/AcceptPersonal/Acepted";
-import Rejected from "../../components/AcceptPersonal/Rejected";
-import OlderPersonal from "../../components/AcceptPersonal/OlderPersonal";
+import { useState } from "react";
+import TableAdmin from "../../components/AcceptPersonal/TableAdmin";
 
 const data = [
   {
@@ -44,60 +42,64 @@ const data = [
 ];
 
 function AceptManagment() {
+  const [tabState, setTabState] = useState(0);
+  const array = [
+    <TableAdmin data={data} key={0} status="Pending" />,
+    <TableAdmin data={data} key={1} status="Acepted" />,
+    <TableAdmin data={data} key={2} status="Rejected" />,
+    <TableAdmin data={data} key={3} status="OlderPersonal" />,
+  ];
+
   return (
-    <>
-      <div className="flex items-center justify-center h-screen">
-        <div role="tablist" className="tabs tabs-boxed w-100 h-15 shadow-xl">
+    <div className="flex flex-col items-center justify-center">
+      <div className="flex items-start justify-center mt-8">
+        <div
+          role="tablist"
+          className="tabs tabs-boxed w-[20rem] md:w-[43rem] h-15 shadow-xl flex flex-col md:flex-row items-center justify-center border-primary border p-0 md:p-[1.5rem]"
+        >
           <input
             type="radio"
             name="my_tabs_1"
             role="tab"
             className="tab"
             aria-label="Pendentes por aprobar"
+            onClick={() => setTabState(0)}
           />
-          <div role="tabpanel" className="tab-content p-5">
-            {/*  <BarTitle title={`Listado de pendentes`} />*/}
-            <Pending data={data} />
-          </div>
+
           <input
             type="radio"
             name="my_tabs_1"
             role="tab"
             className="tab"
             aria-label="Aceptados"
+            onClick={() => setTabState(1)}
             checked
           />
-          <div role="tabpanel" className="tab-content p-10">
-            {/* <BarTitle title={`Listado de aceptados`} /> */}
 
-            <Acepted data={data} />
-          </div>
           <input
             type="radio"
             name="my_tabs_1"
             role="tab"
             className="tab"
             aria-label="Usuarios denegados"
+            onClick={() => setTabState(2)}
           />
-          <div role="tabpanel" className="tab-content p-10">
-            {/* <BarTitle title={`Listado de Denegados`} />*/}
 
-            <Rejected data={data} />
-          </div>
           <input
             type="radio"
             name="my_tabs_1"
             role="tab"
             className="tab"
             aria-label="Ex residentes"
+            onClick={() => setTabState(3)}
           />
-          <div role="tabpanel" className="tab-content p-10">
-            {/* <BarTitle title={`Listado de ex-residentes`} />*/}
-            <OlderPersonal data={data} />
-          </div>
         </div>
       </div>
-    </>
+
+      <div className="p-10 overflow-x-visible md:overflow-none w-[26rem] md:w-[51.2rem]">
+        {array[tabState]}
+      </div>
+    </div>
   );
 }
 
