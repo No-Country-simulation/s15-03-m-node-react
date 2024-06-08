3b3a4e63b385cadf2224cce0be6df9b5092a6a65
Lucas <66272629+MixDevCode@users.noreply.github.com>
2024-06-07 21:43:02 -0300

    prueba de cors numero 56712

--- server/src/app.js
+++ server/src/app.js
@@ -37,9 +37,15 @@ router.use("/zonas", zonasRoute);
 
 app.use('/api', router);
 
-app.options("*", cors({ origin: 'http://localhost:5173', optionsSuccessStatus: 200 }));
+//configuramos cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header("Access-Control-Allow-Headers", "x-access-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
-app.use(cors({ origin: "http://localhost:5173", optionsSuccessStatus: 200 }));
+  app.use(cors());
 //habilamos las rutas
 
 // levantamos el servidor
