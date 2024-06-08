69dcb6632a55ee167738e70d477fbd82745381cc
Lucas <66272629+MixDevCode@users.noreply.github.com>
2024-06-07 21:00:10 -0300

    cors agregado

--- server/src/app.js
+++ server/src/app.js
@@ -2,8 +2,6 @@
 const express = require('express')
 // importamos path
 const path = require('path')
-// importamos cors
-const cors = require('cors')
 
 const router = express.Router();
 
@@ -37,7 +35,16 @@ router.use("/zonas", zonasRoute);
 app.use('/api', router);
 
 //configuramos cors
-app.use(cors())
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 //habilamos las rutas
 
