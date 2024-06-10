07f110dbacb1e463649ba18eb72bd32fa8eba5db
Facundo-Oliva <facundooliva500@hotmail.com>
2024-06-10 12:36:22 -0300

    fixed proyect, add basic protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-import { ContextProvider } from "./context/Context";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { ContextProvider, UseContext } from "./context/Context";
 import Home from "./pages/Home";
 import Error404 from "./pages/Error404";
 import Board from "./pages/BusinessBoard";
@@ -16,23 +16,84 @@ import AceptManagment from "./pages/Admin/AceptManagment";
 import IncidentReport from "./pages/IncidentReport/IncidentReport";
 
 function App() {
+  const { userCreated } = UseContext;
+
+  const RequireAuth = ({ children }) => {
+    return userCreated ? children : <Navigate to="/login" />;
+  };
+
   return (
     <ContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/board" element={<Board />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register-admin" element={<Register isAdmin />} />
           <Route path="/register-resident" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/welcome" element={<WelcomeResident />} />
-          <Route path="/welcome-navigation" element={<WelcomeNavigation />} />
-          <Route path="/reservation" element={<Reservation />} />
-          <Route path="/list-admin" element={<AceptManagment />} />
-          <Route path="/profile" element={<Profile />} /> {/* /:idUser */}
-          <Route path="/neigbor-group" element={<NeigborGroup />} />
-          <Route path="/incident-report" element={<IncidentReport />} />
+          <Route
+            path="/board"
+            element={
+              <RequireAuth>
+                <Board />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/welcome"
+            element={
+              <RequireAuth>
+                <WelcomeResident />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/welcome-navigation"
+            element={
+              <RequireAuth>
+                <WelcomeNavigation />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/list-admin"
+            element={
+              <RequireAuth>
+                <AceptManagment />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/profile" // /:idUser
+            element={
+              <RequireAuth>
+                <Profile />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/neigbor-group"
+            element={
+              <RequireAuth>
+                <NeigborGroup />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/incident-report"
+            element={
+              <RequireAuth>
+                <IncidentReport />
+              </RequireAuth>
+            }
+          />
           <Route path="*" element={<Error404 />} />
         </Route>
       </Routes>
